f924f4c420d773c09b45c46655a39303c2cb2a0c
pjh4400 <vito822@naver.com>
2022-03-15 02:14:26 +0900

    feat: Error 처리 관련 클래스 추가

---
+++ server/src/main/java/com/oddok/server/common/errors/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.oddok.server.common.errors;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(Long id) {
+        super("User not found: " + id);
+    }
+
+    public UserNotFoundException() {
+        super("User not found");
+    }
+}
