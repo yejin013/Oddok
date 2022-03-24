5da82859a059939e00a8f9552cbaac307a5445ee
yejin013 <yejin013@naver.com>
2022-03-24 15:23:43 +0900

    feat: UserNotPublisherException에 Forbidden 추가

--- server/src/main/java/com/oddok/server/common/errors/UserNotPublisherException.java
+++ server/src/main/java/com/oddok/server/common/errors/UserNotPublisherException.java
@@ -1,5 +1,9 @@
 package com.oddok.server.common.errors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class UserNotPublisherException extends RuntimeException {
 
     public UserNotPublisherException() {
