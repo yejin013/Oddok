ecb0cae814fb0a9c5e7660978628c642dabd942b
juhee <77ooee@gmail.com>
2022-03-26 03:51:10 +0900

    chore: Recoil 설정

---
+++ client/src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
