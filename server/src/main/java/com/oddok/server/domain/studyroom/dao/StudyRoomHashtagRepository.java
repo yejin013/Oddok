f242082088bc5d7b0869097192a5ff6eb9dac5ed
evie <evie@gravylab.co.kr>
2022-03-29 18:59:33 +0900

    feat: StudyRoom 수정 시 hashtag 수정

--- server/src/main/java/com/oddok/server/domain/studyroom/dao/StudyRoomHashtagRepository.java
+++ server/src/main/java/com/oddok/server/domain/studyroom/dao/StudyRoomHashtagRepository.java
@@ -1,9 +1,19 @@
 package com.oddok.server.domain.studyroom.dao;
 
+import com.oddok.server.domain.studyroom.entity.Hashtag;
+import com.oddok.server.domain.studyroom.entity.StudyRoom;
 import com.oddok.server.domain.studyroom.entity.StudyRoomHashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudyRoomHashtagRepository extends JpaRepository<StudyRoomHashtag, Long> {
+
+    List<StudyRoomHashtag> findAllByStudyRoom(StudyRoom studyRoom);
+
+    void deleteAllByStudyRoom(StudyRoom studyRoom);
+
+    List<StudyRoomHashtag> findAllByHashtag(Hashtag hashtag);
 }
