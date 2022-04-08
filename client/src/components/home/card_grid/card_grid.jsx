f199a533449a69febd6eacea1fd65c48ee2dacc0
juhee <77ooee@gmail.com>
2022-04-09 01:22:28 +0900

    feat: 스터디룸 리스트 반응형 레이아웃 구현
    Related to: #32

---
+++ client/src/components/home/card_grid/card_grid.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import StudyRoomCard from "../studyroom_card/studyroom_card";
+
+import styles from "./card_grid.module.css";
+
+function CardGrid() {
+  const studyroomList = []; // *임시* 나중에 데이터 받아와서 jsx에서 map으로
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < 16; i++) {
+    studyroomList.push(
+      <Link to="/" className={styles.studyroom}>
+        <StudyRoomCard />
+      </Link>,
+    );
+  }
+  return <section className={styles.container}>{studyroomList}</section>;
+}
+
+export default CardGrid;
