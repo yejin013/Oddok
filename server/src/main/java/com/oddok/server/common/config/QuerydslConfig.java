bed31a80f31e56916828f2fa200c1a88ded489bb
pjh4400 <vito822@naver.com>
2022-04-11 18:25:54 +0900

    chore: querydsl 의존성 추가

---
+++ server/src/main/java/com/oddok/server/common/config/QuerydslConfig.java
@@ -0,0 +1,23 @@
+package com.oddok.server.common.config;
+
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class QuerydslConfig {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Bean
+  public JPAQueryFactory jpaQueryFactory() {
+    return new JPAQueryFactory(entityManager);
+  }
+
+}
