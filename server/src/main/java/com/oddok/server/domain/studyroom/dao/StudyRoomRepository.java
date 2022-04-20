4c705f017fbb7f24fbf472d38124b56bb84ebace
pjh4400 <vito822@naver.com>
2022-04-20 17:44:13 +0900

    refactor: 사용하지 않는 메소드 정리

--- server/src/main/java/com/oddok/server/domain/studyroom/dao/StudyRoomRepository.java
+++ server/src/main/java/com/oddok/server/domain/studyroom/dao/StudyRoomRepository.java
@@ -1,9 +1,6 @@
 package com.oddok.server.domain.studyroom.dao;
 
-import com.oddok.server.domain.studyroom.entity.Category;
 import com.oddok.server.domain.studyroom.entity.StudyRoom;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,10 +10,4 @@ import java.util.Optional;
 @Repository
 public interface StudyRoomRepository extends JpaRepository<StudyRoom, Long> {
     Optional<StudyRoom> findByIdAndEndAtAfter(Long id, LocalDate endAt);
-    Page<StudyRoom> findAllByEndAtAfter(LocalDate endAt, Pageable pageable);
-    Page<StudyRoom> findAllByEndAtAfterAndCategory(LocalDate endAt, Category category, Pageable pageable);
-    Page<StudyRoom> findAllByEndAtAfterAndIsPublicTrue(LocalDate endAt, Pageable pageable);
-    Page<StudyRoom> findAllByEndAtAfterAndCategoryAndIsPublicTrue(LocalDate endAt, Category category, Pageable pageable);
-    Page<StudyRoom> findAllByEndAtAfterAndNameContaining(LocalDate endAt, String name, Pageable pageable);
-    Page<StudyRoom> findAllByEndAtAfterAndNameContainingAndIsPublicTrue(LocalDate endAt, String name, Pageable pageable);
 }
