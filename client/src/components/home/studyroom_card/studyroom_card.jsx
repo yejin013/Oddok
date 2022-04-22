9c7b26f435d7d930f62e4201c459f9dc5d0d2c40
juhee <77ooee@gmail.com>
2022-04-23 01:25:31 +0900

    design: 스터디룸 카드 디자인 수정
    related to: #61

--- client/src/components/home/studyroom_card/studyroom_card.jsx
+++ client/src/components/home/studyroom_card/studyroom_card.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Thumbnail from "./thumbnail";
 import { ReactComponent as Lock } from "../../../assets/icons/lock.svg";
 import { ReactComponent as Unlock } from "../../../assets/icons/unlock.svg";
+import { ReactComponent as BookMark } from "../../../assets/icons/bookmark.svg";
 
 import styles from "./studyroom_card.module.css";
 import UserCount from "./user_count";
@@ -11,21 +12,24 @@ function StudyRoomCard({ roomData }) {
     <li className={styles.container}>
       <div className={styles.thumbnail_box}>
         <Thumbnail />
-        <div className={styles.lock_icon}>{roomData.isPublic ? <Unlock /> : <Lock />}</div>
+        <div className={styles.bookmark_icon}>
+          <BookMark />
+        </div>
         <div className={styles.user_count}>
           <UserCount number={roomData.limitUsers} />
+          <span>/ {roomData.limitUsers}</span>
         </div>
       </div>
       <div className={styles.content_box}>
-        <span className={styles.title}>{roomData.name}</span>
+        <div className={styles.content_head}>
+          <div className={styles.title}>{roomData.name}</div>
+          <div className={styles.lock_icon}>{roomData.isPublic ? <Unlock /> : <Lock />}</div>
+        </div>
         <div>
           {roomData.hashtags.map((hashtag) => (
             <span>#{hashtag} </span>
           ))}
         </div>
-        <span>
-          {roomData.currentUsers}/{roomData.limitUsers}
-        </span>
       </div>
     </li>
   );
