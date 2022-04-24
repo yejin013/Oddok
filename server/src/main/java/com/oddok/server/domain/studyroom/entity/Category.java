d07a7a71e0e3c41c123c23782ee66bd103dc8d49
pjh4400 <vito822@naver.com>
2022-04-24 16:05:37 +0900

    feat: 스터디룸 카테고리 추가

--- server/src/main/java/com/oddok/server/domain/studyroom/entity/Category.java
+++ server/src/main/java/com/oddok/server/domain/studyroom/entity/Category.java
@@ -2,7 +2,7 @@ package com.oddok.server.domain.studyroom.entity;
 
 public enum Category {
     OFFICIAL("공시생"), SCHOOL("대입"), CERTIFICATE("자격증"),
-    EMPLOYEE("취준생"), ETC("개인학습");
+    EMPLOYEE("취준생"), PERSONAL("개인학습"), ETC("일반");
 
     private final String value;
 
