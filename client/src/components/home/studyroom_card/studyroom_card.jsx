c60fd84a8724072e4c81959b587106c72252815c
juhee <77ooee@gmail.com>
2022-04-24 18:20:32 +0900

    feat: 북마크 버튼 클릭시 아이콘 변경
    related to: #61

--- client/src/components/home/studyroom_card/studyroom_card.jsx
+++ client/src/components/home/studyroom_card/studyroom_card.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from "react";
 import Thumbnail from "./thumbnail";
 import UserCount from "./user_count";
 import { ReactComponent as Lock } from "../../../assets/icons/lock.svg";
 import { ReactComponent as Unlock } from "../../../assets/icons/unlock.svg";
 import { ReactComponent as BookMark } from "../../../assets/icons/bookmark.svg";
+import { ReactComponent as BookMarkHeart } from "../../../assets/icons/bookmark-heart-fill.svg";
 
 import styles from "./studyroom_card.module.css";
 
 function StudyRoomCard({ roomData }) {
+  const [isBookMark, setIsBookMark] = useState(false);
+  const addBookMark = (e) => {
+    e.preventDefault();
+    setIsBookMark((prev) => !prev);
+    console.log("북마크 버튼 클릭🥳");
+  };
+
   return (
     <li key={roomData.id} className={styles.container}>
       <div className={styles.wrapper}>
         <div className={styles.thumbnail_box}>
           <Thumbnail />
-          <div className={styles.bookmark_icon}>
-            <BookMark />
+          <div className={styles.bookmark_icon} onClick={addBookMark}>
+            {!isBookMark ? <BookMark /> : <BookMarkHeart />}
           </div>
           <div className={styles.user_count}>
             <UserCount number={roomData.limitUsers} />
