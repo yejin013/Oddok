571e54dcb9953cd3c01cbcf956a6d2ce5ae58702
juhee <77ooee@gmail.com>
2022-05-10 21:07:37 +0900

    feat: 쿼리스트링으로 검색값 가져오기

--- client/src/components/search/SearchResult/SearchResult.jsx
+++ client/src/components/search/SearchResult/SearchResult.jsx
@@ -1,19 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import parseQueryString from "../../../utils/parseQueryString";
 import StudyRoomList from "../../home/studyroom_list/studyroom_list";
 import styles from "./SearchResult.module.css";
 
-function SearchResult({ searchedTitle, searchedHashtag }) {
+function SearchResult() {
   const location = useLocation();
+  const [searchedKeyword, setSearchedKeyword] = useState();
+
+  useEffect(() => {
+    const qs = parseQueryString(location.search);
+    setSearchedKeyword(qs);
+  }, [location.search]);
 
   return (
     <div className={styles.container}>
-      <h2>&ldquo;{location.search}&rdquo; 검색 결과</h2>
+      <h2>&ldquo;{searchedKeyword?.title || searchedKeyword?.hashtag}&rdquo; 검색 결과</h2>
       {
         // TODO 검색결과별 인기 해시태그 필터 추가
         // <h3>태그 필터</h3>
       }
-      <StudyRoomList searchedTitle={searchedTitle} searchedHashtag={searchedHashtag} />
+      <StudyRoomList searchedTitle={searchedKeyword?.title} searchedHashtag={searchedKeyword?.hashtag} />
     </div>
   );
 }
