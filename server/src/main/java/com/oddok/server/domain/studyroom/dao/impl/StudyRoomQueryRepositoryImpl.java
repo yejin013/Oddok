61a9983e7abdd63087c48aa1745ecb0b1e67b6fe
pjh4400 <vito822@naver.com>
2022-05-15 14:26:10 +0900

    refactor: 스터디룸 마감일 비교 쿼리 수정 및 메소드화

--- server/src/main/java/com/oddok/server/domain/studyroom/dao/impl/StudyRoomQueryRepositoryImpl.java
+++ server/src/main/java/com/oddok/server/domain/studyroom/dao/impl/StudyRoomQueryRepositoryImpl.java
@@ -30,9 +30,8 @@ public class StudyRoomQueryRepositoryImpl implements StudyRoomQueryRepository {
 
     public List<StudyRoom> findAllBySearchConditions(Boolean isPublic, String category, String name,
                                                      Pageable pageable) {
-        LocalDate now = LocalDate.now();
         JPAQuery<StudyRoom> query = queryFactory.selectFrom(studyRoom)
-                .where(studyRoom.endAt.eq(now).or(studyRoom.endAt.after(now)),
+                .where(notEnd(),
                         eqIsPublic(isPublic),
                         eqCategory(category),
                         containsName(name))
@@ -44,11 +43,10 @@ public class StudyRoomQueryRepositoryImpl implements StudyRoomQueryRepository {
 
     public List<StudyRoom> findAllByHashtag(Boolean isPublic, String category, Hashtag hashtag,
                                                      Pageable pageable) {
-        LocalDate now = LocalDate.now();
         JPAQuery<StudyRoom> query = queryFactory.selectFrom(studyRoom)
                 .innerJoin(studyRoomHashtag)
                 .on(studyRoom.eq(studyRoomHashtag.studyRoom))
-                .where(studyRoom.endAt.eq(now).or(studyRoom.endAt.after(now)),
+                .where(notEnd(),
                         eqIsPublic(isPublic),
                         eqCategory(category),
                         studyRoomHashtag.hashtag.eq(hashtag))
@@ -58,11 +56,13 @@ public class StudyRoomQueryRepositoryImpl implements StudyRoomQueryRepository {
         return query.fetch();
     }
 
-    public Optional<StudyRoom> findByIdAndEndAtIsEqualOrAfter(Long id) {
+    private BooleanExpression notEnd() {
+        return studyRoom.endAt.goe(LocalDate.now());
+    }
 
-        LocalDate now = LocalDate.now();
+    public Optional<StudyRoom> findByIdAndEndAtIsEqualOrAfter(Long id) {
         JPAQuery<StudyRoom> query = queryFactory.selectFrom(studyRoom)
-                .where(studyRoom.endAt.eq(now).or(studyRoom.endAt.after(now)),
+                .where(notEnd(),
                         studyRoom.id.eq(id));
         return Optional.ofNullable(query.fetchOne());
     }
