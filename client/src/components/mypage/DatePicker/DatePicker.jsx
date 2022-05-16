24b45cf9f8ca91694e27112f3b4d7dc20492bba1
juhee <77ooee@gmail.com>
2022-05-16 23:25:46 +0900

    feat: date format 수정

--- client/src/components/mypage/DatePicker/DatePicker.jsx
+++ client/src/components/mypage/DatePicker/DatePicker.jsx
@@ -12,26 +12,19 @@ import styles from "./DatePicker.module.css";
 function DatePicker({ setSelectedDate }) {
   const [currentDate, setCurrentDate] = useState(new Date());
 
-  const prevDay = () => {
-    setCurrentDate(subDays(currentDate, 1));
-  };
-  const nextDay = () => {
-    setCurrentDate(addDays(currentDate, 1));
-  };
-
   useEffect(() => {
     setSelectedDate(currentDate.toISOString().slice(0, 10));
   }, [currentDate]);
 
   return (
     <div className={styles.box}>
-      <button type="button" className={styles.move_button} onClick={prevDay}>
+      <button type="button" className={styles.move_button} onClick={() => setCurrentDate(subDays(currentDate, 1))}>
         <ArrowLeft />
       </button>
       <div className={styles.date_picker}>
         <Picker
           locale={ko}
-          dateFormat="yyyy/MM/dd"
+          dateFormat="yyyy.MM.dd (E)"
           selected={currentDate}
           onChange={(date) => setCurrentDate(date)}
           minDate={new Date()}
@@ -47,7 +40,7 @@ function DatePicker({ setSelectedDate }) {
           }
         />
       </div>
-      <button type="button" className={styles.move_button} onClick={nextDay}>
+      <button type="button" className={styles.move_button} onClick={() => setCurrentDate(addDays(currentDate, 1))}>
         <ArrowRight />
       </button>
     </div>
