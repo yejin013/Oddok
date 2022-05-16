9bea7110945640f6e588ed8f5d7f8f7fc2a53caa
pjh4400 <vito822@naver.com>
2022-05-16 16:03:19 +0900

    fix: api url 수정

--- server/src/main/java/com/oddok/server/domain/studyroom/api/HashtagController.java
+++ server/src/main/java/com/oddok/server/domain/studyroom/api/HashtagController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hashtags")
+@RequestMapping("/hashtag")
 @RequiredArgsConstructor
 public class HashtagController {
 
