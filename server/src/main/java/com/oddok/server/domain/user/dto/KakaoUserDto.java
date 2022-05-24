b41955c2e246f0c7bc0b244fed366e972f16ca3f
pjh4400 <vito822@naver.com>
2022-05-24 23:07:45 +0900

    fix: 카카오 사용자 데이터 받아오기 요청 성공 / 파싱 안됨

--- server/src/main/java/com/oddok/server/domain/user/dto/KakaoUserDto.java
+++ server/src/main/java/com/oddok/server/domain/user/dto/KakaoUserDto.java
@@ -1,15 +1,25 @@
 package com.oddok.server.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KakaoUserDto {
-    private final Long id;
-    private final Properties properties;
-    private final KakaoAccount kakaoAccount;
+    private Long id;
+    private Properties properties;
+    private KakaoAccount kakaoAccount;
+
+    @JsonCreator
+    public KakaoUserDto(Long id, Properties properties, KakaoAccount kakaoAccount) {
+        this.id = id;
+        this.properties = properties;
+        this.kakaoAccount = kakaoAccount;
+    }
 
     @ToString
     @Getter
