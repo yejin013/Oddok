423d616de90b9e6c0b726bf2ba04d1cdd63adbd2
juhee <77ooee@gmail.com>
2022-05-25 01:35:21 +0900

    feat: 닉네임 수정 모달 컴포넌트 제작 related to: #109

--- client/src/components/mypage/EditModal/NicknameEditModal.jsx
+++ client/src/components/mypage/EditModal/NicknameEditModal.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { Modal } from "@components/commons";
+import { Modal, Input } from "@components/commons";
+import styles from "./NicknameEditModal.module.css";
 
 function NicknameEditModal() {
-  return <div>NicknameEditModal</div>;
+  const content = (
+    <div className={styles.box}>
+      <p>닉네임</p>
+      <Input />
+    </div>
+  );
+  return <Modal title="닉네임 수정" content={content} onAction={{ text: "확인" }} />;
 }
 
 export default NicknameEditModal;
