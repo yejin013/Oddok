42d36682b1e26cbbe7d518af345e0e3184116854
jiwon Nam <swm10099@naver.com>
2022-05-25 22:53:40 +0900

    feat: 전체 스터디원 api추가

--- client/src/pages/MainHome/MainHome.jsx
+++ client/src/pages/MainHome/MainHome.jsx
@@ -3,6 +3,7 @@ import { useSetRecoilState } from "recoil";
 import { bookmarkState } from "@recoil/bookmark-state";
 import { getBookmark } from "@api/bookmark-api";
 import { getTestUser } from "@api/getTestUser";
+import { getTotalParticipant } from "@api/participant-api";
 import { Bookmark, Footer, Header, StudyRoomList, TotalParticipant } from "@components/home";
 import styles from "./MainHome.module.css";
 
@@ -16,16 +17,22 @@ function MainHome() {
       .catch((error) => console.log("get user error", error));
   }, []);
 
+  useEffect(() => {
+    // if (로그인을 안했다면)
+    getTotalParticipant()
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error.response));
+  }, []);
+
   const showBookmark = async () => {
     await getBookmark()
-      .then((response) => setBookmark(response))
+      .then((response) => setBookmark("전체참여자수", response))
       .catch((error) => console.log("get bookmark error", error));
   };
 
   return (
     <div className={styles.home}>
       <Header />
-      <TotalParticipant />
       <Bookmark showBookmark={showBookmark} />
       <section className={styles.studyroom_list}>
         <h2>STUDY ROOM</h2>
