185796622c112784dcf40deac706466833e87506
jiwon Nam <swm10099@naver.com>
2022-05-25 22:11:21 +0900

    feat: 스터디원 6명 이상일 경우 스타일 추가

--- client/src/components/commons/UserCount/UserCount.jsx
+++ client/src/components/commons/UserCount/UserCount.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { Person } from "@icons";
+import { Plus, Person } from "@icons";
 import styles from "./UserCount.module.css";
 
 function UserCount({ number, isBookmarkUser }) {
   const users = [];
 
   for (let i = 0; i < number; i += 1) {
-    users.push(
-      <div className={isBookmarkUser ? styles.bookmark_user : styles.user}>
-        <Person />
-      </div>,
-    );
+    // 참여자 6명 이상일때 옆에 + 하나만 추가
+    if (i > 4 && i <= 5) {
+      users.push(
+        <div className={`${styles.plus} ${isBookmarkUser ? styles.bookmark_user : styles.user}`}>
+          <Plus />
+        </div>,
+      );
+    } else if (i <= 4) {
+      users.push(
+        <div className={isBookmarkUser ? styles.bookmark_user : styles.user}>
+          <Person />
+        </div>,
+      );
+    }
   }
 
   return <div className={styles.count}>{users}</div>;
