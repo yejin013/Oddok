416206c4d7c120f06203bdbc912a6bcef5d84ad5
pjh4400 <vito822@naver.com>
2022-06-20 23:02:26 +0900

    fix: 타임존 설정

--- server/src/main/java/com/oddok/server/ServerApplication.java
+++ server/src/main/java/com/oddok/server/ServerApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class ServerApplication {
+    @PostConstruct
+    public void initTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
