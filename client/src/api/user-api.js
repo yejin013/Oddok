283802b8c9550323b5c3359b5c0461278121c7b5
jiwon Nam <swm10099@naver.com>
2022-07-01 17:21:31 +0900

    feat: 마이페이지 닉네임 수정 기능 추가

--- client/src/api/user-api.js
+++ client/src/api/user-api.js
@@ -1,6 +1,14 @@
+import axios from "axios";
 import axiosInstance from "./axios-config";
 
 export const getNickname = async () => {
   const response = await axiosInstance.get("/user/nickname");
+  return response;
+};
+
+export const editNickname = async (nickname) => {
+  const response = axiosInstance.patch("/user/nickname", {
+    nickname,
+  });
   return response;
 };
--- client/src/components/commons/Modal/NicknameEditModal.jsx
+++ client/src/components/commons/Modal/NicknameEditModal.jsx
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Modal, Input } from "@components/commons";
+import { editNickname } from "@api/user-api";
+import { useRecoilState } from "recoil";
+import { userState } from "@recoil/user-state";
 import styles from "./NicknameEditModal.module.css";
 
 function NicknameEditModal({ onClose }) {
+  const [user, setUserState] = useRecoilState(userState);
+  const [nickname, setNickname] = useState(user.nickname);
+
+  const onChange = (event) => {
+    if (event.currentTarget == null) {
+      return;
+    }
+    event.preventDefault();
+    setNickname(event.currentTarget.value);
+  };
+
+  const changeNickname = async (name) => {
+    const response = await editNickname(name);
+    setUserState((prev) => ({ ...prev, nickname: response.nickname }));
+  };
+
   const content = (
     <div className={styles.box}>
       <p>닉네임</p>
-      <Input />
+      <Input value={nickname} onChange={onChange} />
     </div>
   );
-  return <Modal title="닉네임 수정" content={content} onClose={onClose} onAction={{ text: "확인" }} />;
+
+  return (
+    <Modal
+      title="닉네임 수정"
+      content={content}
+      onClose={onClose}
+      onAction={{
+        text: "확인",
+        action: async () => {
+          changeNickname(nickname);
+          onClose();
+        },
+      }}
+    />
+  );
 }
 
 export default NicknameEditModal;
