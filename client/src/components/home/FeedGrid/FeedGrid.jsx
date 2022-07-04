6e39d62a5030bb4cdc77c9181925d26c42491f27
juhee <77ooee@gmail.com>
2022-07-04 16:58:24 +0900

    design: 검색 결과 없음 문구 및 디자인 수정

--- client/src/components/home/FeedGrid/FeedGrid.jsx
+++ client/src/components/home/FeedGrid/FeedGrid.jsx
@@ -7,17 +7,25 @@ import styles from "./FeedGrid.module.css";
 
 function CardGrid({ isLoading, rooms, showBookmark }) {
   return (
-    <ul className={styles.container}>
-      {rooms?.map((roomData) => (
-        <Link key={roomData.id} to={`/studyroom/${roomData.id}/setting`} className={styles.studyroom}>
-          <StudyRoomCard //
-            roomData={roomData}
-            showBookmark={showBookmark}
-          />
-        </Link>
-      ))}
-      {isLoading && new Array(16).fill(0).map(() => <Skeleton />)}
-    </ul>
+    <div className={styles.content}>
+      <ul>
+        {isLoading && new Array(16).fill(0).map(() => <Skeleton />)}
+        {rooms?.map((roomData) => (
+          <Link key={roomData.id} to={`/studyroom/${roomData.id}/setting`} className={styles.studyroom}>
+            <StudyRoomCard //
+              roomData={roomData}
+              showBookmark={showBookmark}
+            />
+          </Link>
+        ))}
+      </ul>
+      {!isLoading && rooms.length === 0 && (
+        <div className={styles.empty}>
+          <EmojiDizzy />
+          <p>찾으시는 스터디룸이 없습니다.</p>
+        </div>
+      )}
+    </div>
   );
 }
 
