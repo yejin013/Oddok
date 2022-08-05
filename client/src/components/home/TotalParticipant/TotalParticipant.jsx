b0f2a4c8f201adb67894e36d57b3da89de8a2228
jiwon Nam <swm10099@naver.com>
2022-08-05 19:37:04 +0900

    refactor: 전체참여자수 컴포넌트 리팩토링

--- client/src/components/home/TotalParticipant/TotalParticipant.jsx
+++ client/src/components/home/TotalParticipant/TotalParticipant.jsx
@@ -9,46 +9,34 @@ import styles from "./TotalParticipant.module.css";
 function TotalParticipant() {
   const user = useRecoilValue(userState);
   const history = useHistory();
-  const [totalParticipant, setTotalParticipant] = useState();
+  const [totalParticipant, setTotalParticipant] = useState(0);
 
   useEffect(() => {
     getTotalParticipant()
       .then((response) => setTotalParticipant(response.data))
-      .catch((error) => console.log("get total participant error", error));
+      .catch((error) => console.error(error));
   }, []);
 
+  const onButtonClick = () => {
+    if (!user.isLogin) {
+      history.push("/login");
+      return;
+    }
+    history.push("/studyroom/create");
+  };
+
   return (
-    <div className={styles.participant}>
+    <section className={styles.participant}>
+      <div className={styles.count_icon}>
+        <UserCount number={totalParticipant} />
+      </div>
       <div className={styles.count_box}>
-        <div className={styles.count_icon}>
-          <UserCount number={totalParticipant} />
-        </div>
-        <p className={styles.count}>{totalParticipant}명이 ODDOK에서 공부 중이에요</p>
+        <span className={styles.text}>{totalParticipant}명이 ODDOK에서 공부 중이에요</span>
+        <button className={styles.button} type="button" onClick={onButtonClick}>
+          ODDOK과 함께 스터디 시작하기
+        </button>
       </div>
-      <div className={styles.button_box}>
-        {!user.isLogin ? (
-          <button
-            className={styles.button}
-            type="button"
-            onClick={() => {
-              history.push("/login");
-            }}
-          >
-            ODDOK과 함께 스터디 시작하기
-          </button>
-        ) : (
-          <button
-            className={styles.button}
-            type="button"
-            onClick={() => {
-              history.push("/studyroom/create");
-            }}
-          >
-            ODDOK과 함께 스터디 시작하기
-          </button>
-        )}
-      </div>
-    </div>
+    </section>
   );
 }
 
