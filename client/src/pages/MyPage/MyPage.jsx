dab987b48cfe07fa7dca157bc04bbaf2852f51ab
juhee <77ooee@gmail.com>
2022-08-10 19:59:11 +0900

    chore: paths alias 설정 패키지 변경

--- client/src/pages/MyPage/MyPage.jsx
+++ client/src/pages/MyPage/MyPage.jsx
@@ -16,8 +16,7 @@ import {
 import { Textarea, NicknameEditModal } from "@components/commons";
 import { Layout } from "@components/layout";
 import useAsync from "@hooks/useAsync";
-import getColor from "src/utils/getColor";
-import { getTimeDiff, getDday, dateParsing, dateFormatting } from "@utils";
+import { getColor, getTimeDiff, getDday, dateParsing, dateFormatting } from "@utils";
 import useModal from "@hooks/useModal";
 import styles from "./MyPage.module.css";
 
