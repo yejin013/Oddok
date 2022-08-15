53ee7fc9a9e84673b212ab8e53463f443de14d53
jiwon Nam <swm10099@naver.com>
2022-08-15 21:50:35 +0900

    feat: 모달 안에서 로딩 사용할 수 있도록 수정

--- client/src/components/mypage/AccountDeleteModal/AccountDeleteModal.jsx
+++ client/src/components/mypage/AccountDeleteModal/AccountDeleteModal.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Loading, Modal } from "@components/commons";
+import { Modal } from "@components/commons";
 import { deleteAccount } from "@api/auth/auth-api";
+import useAsync from "@hooks/useAsync";
 import { useRecoilState, useSetRecoilState } from "recoil";
 import { userState } from "@recoil/user-state";
 import { errorState } from "@recoil/error-state";
-import useAsync from "@hooks/useAsync";
 
 function AccountDeleteModal({ onClose }) {
   const [user, setUserState] = useRecoilState(userState);
@@ -20,26 +20,14 @@ function AccountDeleteModal({ onClose }) {
     onError: (error) => setError(error),
   });
 
-  // const onDeleteAccount = () => {
-  //   deleteAccount()
-  //     .then(() => {
-  //       localStorage.setItem("isLogin", false);
-  //       setUserState({ ...user, isLogin: JSON.parse(localStorage.getItem("isLogin")) });
-  //       window.location.replace("/");
-  //     })
-  //     .catch((error) => setError(error));
-  // };
-
   return (
-    <>
-      {isLoading && <Loading />}
-      <Modal
-        title="계정 삭제"
-        content="ODDOK 계정을 삭제하시겠습니까?"
-        onClose={onClose}
-        onAction={{ text: "삭제하기", action: onDeleteAccount }}
-      />
-    </>
+    <Modal
+      title="계정 삭제"
+      content="ODDOK 계정을 삭제하시겠습니까?"
+      onClose={onClose}
+      onAction={{ text: "삭제하기", action: onDeleteAccount }}
+      isLoading={isLoading}
+    />
   );
 }
 
