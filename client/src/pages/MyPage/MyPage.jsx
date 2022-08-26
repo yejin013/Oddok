14605190bb6ad58784184d9f2d46f26bb1f6068e
juhee <77ooee@gmail.com>
2022-08-26 20:59:58 +0900

    feat: 마이페이지 스크롤 이동 메뉴 구현

--- client/src/pages/MyPage/MyPage.jsx
+++ client/src/pages/MyPage/MyPage.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { SideNavBar, MyGoal, StudyTime, MyRoom, MyAccount } from "@components/mypage";
 import { Layout } from "@components/layout";
 import styles from "./MyPage.module.css";
 
 function MyPage() {
+  const indexRef = useRef();
+  const targetRef = useRef();
+
+  useEffect(() => {
+    const indexNodes = [...indexRef.current.children].filter((e) => e.children.length === 0);
+    indexNodes.map((node, i) =>
+      node.addEventListener("click", () => {
+        targetRef.current.children[i].scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }),
+    );
+  }, []);
+
   return (
     <Layout>
       <div className={styles.mypage}>
-        <aside className={styles.side_nav_bar}>
-          <SideNavBar />
-        </aside>
-        <div className={styles.container}>
+        <SideNavBar indexRef={indexRef} />
+        <div ref={targetRef} className={styles.container}>
           <MyGoal />
           <StudyTime />
           <MyRoom />
