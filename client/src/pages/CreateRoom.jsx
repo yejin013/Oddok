f431c3328510b4d01dc3e22e96e0595610cdc251
juhee <77ooee@gmail.com>
2022-08-31 22:21:45 +0900

    refactor: atom에 저장된 스터디룸 정보 리셋 로직 이동

--- client/src/pages/CreateRoom.jsx
+++ client/src/pages/CreateRoom.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import React, { useEffect, useLayoutEffect } from "react";
+import { useRecoilState, useRecoilValue, useResetRecoilState, useSetRecoilState } from "recoil";
 import { userState } from "@recoil/user-state";
 import { roomInfoState } from "@recoil/studyroom-state";
 import { errorState } from "@recoil/error-state";
@@ -9,19 +9,20 @@ import { SettingRoom } from "@components/study";
 import { useGoToPage, useAsync } from "@hooks";
 
 function CreateRoom() {
-  const roomInfo = useRecoilValue(roomInfoState);
   const [userInfo, setUserInfo] = useRecoilState(userState);
+  const roomInfo = useRecoilValue(roomInfoState);
+  const resetRoomInfo = useResetRecoilState(roomInfoState);
   const setError = useSetRecoilState(errorState);
   const { goToStudy } = useGoToPage();
-
   const { loading, request: startStudy } = useAsync({
     requestFn: () => startStudyRoom(roomInfo),
     onSuccess: ({ id, token }) => goToStudy(id, token),
     onError: (error) => setError(error),
   });
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     setUserInfo({ ...userInfo, updateAllowed: true }); // @TODO 방장 권한 부여
+    resetRoomInfo();
   }, []);
 
   return (
